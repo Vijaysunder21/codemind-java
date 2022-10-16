1fb84299176a5d62ded1cbd4b439f68727f3ff03
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-10-16 13:42:27 +0530

    import java.util.Scanner; class Main {   public static void main(String[] args) {     Scanner n=new Scanner(System.in);     int k=n.nextInt();     int l=n.nextInt();     int m=n.nextInt();     System.out.println(k*l*m*1024);   } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner n=new Scanner(System.in);
+    int k=n.nextInt();
+    int l=n.nextInt();
+    int m=n.nextInt();
+    System.out.println(k*l*m*1024);
+  }
+}
