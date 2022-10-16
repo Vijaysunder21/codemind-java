015e403e1b278795c19db18571611d38a1b5545b
Vijaysunder21 <97424648+Vijaysunder21@users.noreply.github.com>
2022-10-16 13:51:40 +0530

    import java.util.Scanner; class Main {   public static void main(String[] args) {     Scanner n=new Scanner(System.in);     float k=n.nextFloat();     float l=n.nextFloat();     System.out.format("%.4f",((k+l)/2));   } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner n=new Scanner(System.in);
+    float k=n.nextFloat();
+    float l=n.nextFloat();
+    System.out.format("%.4f",((k+l)/2));
+  }
+}
